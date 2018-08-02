618f2f205f523cc76f564e327a2e83f54318f8ba
Raoul v. R <vanruesc@users.noreply.github.com>
2018-08-02 15:38:47 +0200

    Version 2.0.4.

--- build/delta-controls.js
+++ build/delta-controls.js
@@ -1,5 +1,5 @@
 /**
- * delta-controls v2.0.3 build Sun Jul 01 2018
+ * delta-controls v2.0.4 build Thu Aug 02 2018
  * https://github.com/vanruesc/delta-controls
  * Copyright 2018 Raoul van Rüschen, Zlib
  */
@@ -1220,6 +1220,12 @@
 				return this.x * v.x + this.y * v.y;
 			}
 		}, {
+			key: "cross",
+			value: function cross(v) {
+
+				return this.x * v.y - this.y * v.x;
+			}
+		}, {
 			key: "manhattanLength",
 			value: function manhattanLength() {
 
@@ -2238,6 +2244,25 @@
 				this.w = r;
 
 				return this.normalize();
+			}
+		}, {
+			key: "angleTo",
+			value: function angleTo(q) {
+
+				return 2.0 * Math.acos(Math.abs(Math.min(Math.max(this.dot(q), -1.0), 1.0)));
+			}
+		}, {
+			key: "rotateTowards",
+			value: function rotateTowards(q, step) {
+
+				var angle = this.angleTo(q);
+
+				if (angle !== 0.0) {
+
+					this.slerp(q, Math.min(1.0, step / angle));
+				}
+
+				return this;
 			}
 		}, {
 			key: "invert",
