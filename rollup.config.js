8a4b469483d9c852fd985b031864d4900e228a08
Raoul v. R <vanruesc@users.noreply.github.com>
2020-01-23 17:10:59 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import babel from "rollup-plugin-babel";
-import minify from "rollup-plugin-babel-minify";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
 const pkg = require("./package.json");
 const date = (new Date()).toDateString();
@@ -8,64 +8,64 @@ const date = (new Date()).toDateString();
 const banner = `/**
  * ${pkg.name} v${pkg.version} build ${date}
  * ${pkg.homepage}
- * Copyright ${date.slice(-4)} ${pkg.author.name}, ${pkg.license}
+ * Copyright ${date.slice(-4)} ${pkg.author.name}
+ * @license ${pkg.license}
  */`;
+
+const production = (process.env.NODE_ENV === "production");
+const external = Object.keys(pkg.peerDependencies).concat(["three"]);
+const globals = Object.assign({}, ...external.map((value) => ({
+	[value]: value.replace(/-/g, "").toUpperCase()
+})));
 
 const lib = {
 
-	input: pkg.module,
-	output: {
-		file: "build/" + pkg.name + ".js",
-		format: "umd",
-		name: pkg.name.replace(/-/g, "").toUpperCase(),
-		banner: banner,
-		globals: { three: "THREE" }
+	module: {
+		input: "src/index.js",
+		external,
+		plugins: [resolve()],
+		output: [{
+			file: pkg.module,
+			format: "esm",
+			globals,
+			banner
+		}, {
+			file: pkg.main,
+			format: "esm",
+			globals
+		}, {
+			file: pkg.main.replace(".js", ".min.js"),
+			format: "esm",
+			globals
+		}]
 	},
 
-	external: ["three"],
-	plugins: [resolve()].concat(process.env.NODE_ENV === "production" ? [babel()] : [])
+	main: {
+		input: production ? pkg.main : "src/index.js",
+		external,
+		plugins: production ? [babel()] : [],
+		output: {
+			file: pkg.main,
+			format: "umd",
+			name: pkg.name.replace(/-/g, "").toUpperCase(),
+			globals,
+			banner
+		}
+	},
+
+	min: {
+		input: pkg.main.replace(".js", ".min.js"),
+		external,
+		plugins: [terser(), babel()],
+		output: {
+			file: pkg.main.replace(".js", ".min.js"),
+			format: "umd",
+			name: pkg.name.replace(/-/g, "").toUpperCase(),
+			globals,
+			banner
+		}
+	}
 
 };
 
-const demo = {
-
-	input: "demo/src/index.js",
-	output: {
-		file: "public/demo/index.js",
-		format: "iife",
-		globals: { three: "THREE" }
-	},
-
-	external: ["three"],
-	plugins: [resolve()].concat(process.env.NODE_ENV === "production" ? [babel()] : [])
-
-};
-
-export default [lib, demo].concat((process.env.NODE_ENV === "production") ? [
-
-	Object.assign({}, lib, {
-
-		output: Object.assign({}, lib.output, {
-			file: "build/" + pkg.name + ".min.js"
-		}),
-
-		plugins: [resolve(), babel(), minify({
-			bannerNewLine: true,
-			comments: false
-		})]
-
-	}),
-
-	Object.assign({}, demo, {
-
-		output: Object.assign({}, demo.output, {
-			file: "public/demo/index.min.js"
-		}),
-
-		plugins: [resolve(), babel(), minify({
-			comments: false
-		})]
-
-	})
-
-] : []);
+export default production ? [lib.module, lib.main, lib.min] : [lib.main];
