003bb3b2445a259b3df1f8acdffa45595c5c7f63
Raoul v. R <vanruesc@users.noreply.github.com>
2020-08-18 21:07:20 +0200

    Update configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
+import buble from "@rollup/plugin-buble";
 import resolve from "@rollup/plugin-node-resolve";
-import babel from "rollup-plugin-babel";
-import { eslint } from "rollup-plugin-eslint";
+import typescript  from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 
 const pkg = require("./package.json");
@@ -20,90 +20,59 @@ const globals = Object.assign({}, ...external.map((value) => ({
 })));
 
 const lib = {
-
 	module: {
-		input: "src/index.js",
+		input: "src/index.ts",
+		plugins: [resolve(), typescript({
+			tsconfig: "tsconfig.lib.json"
+		})],
 		external,
-		plugins: [resolve(), eslint()],
-		output: [{
-			file: pkg.module,
+		output: {
+			dir: "build",
+			entryFileNames: pkg.name + ".esm.js",
 			format: "esm",
-			globals,
 			banner
-		}, {
-			file: pkg.main,
-			format: "esm",
-			globals
-		}, {
-			file: pkg.main.replace(".js", ".min.js"),
-			format: "esm",
-			globals
-		}]
+		}
 	},
-
 	main: {
-		input: pkg.main,
+		input: "src/index.ts",
+		plugins: [resolve(), typescript({
+			tsconfig: "tsconfig.lib.json"
+		}), buble()],
 		external,
-		plugins: [babel()],
-		output: {
-			file: pkg.main,
+		output: [{
+			dir: "build",
+			entryFileNames: pkg.name + ".js",
 			format: "umd",
 			name: pkg.name.replace(/-/g, "").toUpperCase(),
 			globals,
 			banner
-		}
-	},
-
-	min: {
-		input: pkg.main.replace(".js", ".min.js"),
-		external,
-		plugins: [terser(), babel()],
-		output: {
-			file: pkg.main.replace(".js", ".min.js"),
+		}, {
+			dir: "build",
+			entryFileNames: pkg.name + ".min.js",
 			format: "umd",
 			name: pkg.name.replace(/-/g, "").toUpperCase(),
+			plugins: [terser()],
 			globals,
 			banner
-		}
+		}]
 	}
-
 };
 
 const demo = {
-
-	module: {
-		input: "demo/src/index.js",
-		plugins: [resolve(), eslint()],
-		output: [{
-			file: "public/demo/index.js",
-			format: "esm"
-		}].concat(production ? [{
-			file: "public/demo/index.min.js",
-			format: "esm"
-		}] : [])
-	},
-
-	main: {
-		input: production ? "public/demo/index.js" : "demo/src/index.js",
-		plugins: production ? [babel()] : [resolve(), eslint()],
-		output: [{
-			file: "public/demo/index.js",
-			format: "iife"
-		}]
-	},
-
-	min: {
-		input: "public/demo/index.min.js",
-		plugins: [terser(), babel()],
-		output: {
-			file: "public/demo/index.min.js",
-			format: "iife"
-		}
-	}
-
+	input: "demo/src/index.ts",
+	plugins: [resolve(), typescript({
+		tsconfig: "tsconfig.demo.json"
+	})].concat(production ? [buble()] : []),
+	output: [{
+		dir: "public/demo",
+		entryFileNames: "[name].js",
+		format: "iife"
+	}].concat(production ? [{
+		dir: "public/demo",
+		entryFileNames: "[name].min.js",
+		format: "iife",
+		plugins: [terser()]
+	}] : [])
 };
 
-export default production ? [
-	lib.module, lib.main, lib.min,
-	demo.module, demo.main, demo.min
-] : [demo.main];
+export default production ? [lib.module, lib.main, demo]: [demo];
