5a3be6d87b7cbd9bbce31ed4a1e6b3f9d16b76b3
Raoul v. R <vanruesc@users.noreply.github.com>
2020-12-04 23:39:23 +0100

    Simplify index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,5 @@
-export {
-	Action,
-	SpatialControls
-} from "./core";
-
-export {
-	KeyCode,
-	PointerButton
-} from "./input";
-
-export {
-	MovementState,
-	RotationManager,
-	TranslationManager
-} from "./managers";
-
-export {
-	GeneralSettings,
-	KeyBindings,
-	PointerSettings,
-	RotationSettings,
-	SensitivitySettings,
-	TranslationSettings,
-	Settings,
-	ZoomSettings
-} from "./settings";
-
-export {
-	Direction,
-	MovementStrategy,
-	Strategy,
-	ZoomStrategy
-} from "./strategies";
+export * from "./core";
+export * from "./input";
+export * from "./managers";
+export * from "./settings";
+export * from "./strategies";
