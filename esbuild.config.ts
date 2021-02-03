06e2092a83d48fbb7f4bb4364fd8fa17c6f04bd3
Raoul v. R <vanruesc@users.noreply.github.com>
2021-02-03 15:18:21 +0100

    Add require shim

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -13,18 +13,20 @@ const banner = `/**
  * @license ${pkg.license}
  */`;
 
+const requireShim = `if(typeof window === "object"&&!window.require)window.require=()=>window.THREE;`;
 const footer = `if(typeof module==="object"&&module.exports)module.exports=${globalName};`;
 
 function config(infile: string, outfile: string, format: string, minify = false): BuildOptions {
 
 	const lib = (infile === "src/index.ts");
 	const iife = (format === "iife");
+	const header = (!lib || !iife) ? banner : banner + "\n" + requireShim;
 
 	return {
 		entryPoints: [infile],
 		external: lib ? external : [],
 		globalName: lib ? globalName : "",
-		banner: lib ? banner : "",
+		banner: lib ? header : "",
 		footer: (lib && iife) ? footer : "",
 		bundle: true,
 		outfile,
