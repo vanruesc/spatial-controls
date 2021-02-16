78f13c1c8414c9c263daeee5bb2200e2fffad54b
Raoul v. R <vanruesc@users.noreply.github.com>
2021-02-16 16:00:46 +0100

    Create ControlMode.ts

---
+++ src/core/ControlMode.ts
@@ -0,0 +1,19 @@
+/**
+ * An enumeration of control modes.
+ */
+
+export enum ControlMode {
+
+	/**
+	 * First person mode.
+	 */
+
+	FIRST_PERSON = "first-person",
+
+	/**
+	 * Third person mode, AKA orbiting.
+	 */
+
+	THIRD_PERSON = "third-person"
+
+}
