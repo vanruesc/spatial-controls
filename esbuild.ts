6db7015918c0c809c25795bf9b659684ebc98194
Raoul v. R <vanruesc@users.noreply.github.com>
2021-02-16 16:00:19 +0100

    Update esbuild.ts

--- esbuild.ts
+++ esbuild.ts
@@ -2,49 +2,44 @@ import { startService } from "esbuild";
 import { watch } from "chokidar";
 import * as path from "path";
 import * as yargs from "yargs";
-import { configLists, sourceDirectories } from "./esbuild.config";
+import { configs, srcDirs } from "./esbuild.config";
 
 const { argv } = yargs.options({ watch: { alias: "w", type: "boolean" } });
 
 async function build(changedFile: string = null): Promise<void> {
 
 	const service = await startService();
+	const t0 = Date.now();
+	const promises: Promise<void>[] = configs.map((c) => {
 
-	for(const configs of configLists) {
+		let p: Promise<void> = null;
 
-		const t0 = Date.now();
-		const promises: Promise<void>[] = configs.map((c) => {
+		if(path.normalize(c.outfile) !== changedFile) {
 
-			let p: Promise<void> = null;
+			p = service.build(c).then((result) => {
 
-			if(path.normalize(c.outfile) !== changedFile) {
+				console.log(`Built ${c.outfile} in ${Date.now() - t0}ms`);
+				result.warnings.forEach((w) => console.warn(w.text,
+					`${w.location.line}:${w.location.column} ${w.location.file}`));
 
-				p = service.build(c).then((result) => {
+			}).catch((e) => console.error(`Failed to build ${c.outfile}`));
 
-					console.log(`Built ${c.outfile} in ${Date.now() - t0}ms`);
-					result.warnings.forEach((w) => console.warn(w.text,
-						`${w.location.line}:${w.location.column} ${w.location.file}`));
+		}
 
-				}).catch((e) => console.error(`Failed to build ${c.outfile}`));
+		return p;
 
-			}
+	});
 
-			return p;
-
-		});
-
-		await Promise.all(promises);
-
-	}
+	await Promise.all(promises);
 
 }
 
 if(argv.watch) {
 
 	console.log("Watching %s for changes…\n",
-		sourceDirectories.join(", ").replace(/, ([^,]*)$/, " and $1"));
+		srcDirs.join(", ").replace(/, ([^,]*)$/, " and $1"));
 
-	const watcher = watch(sourceDirectories);
+	const watcher = watch(srcDirs);
 	watcher.on("change", (f: string) => void build(path.normalize(f)));
 
 }
