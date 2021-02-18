203847ec8ba033b0a6773195b30f2f77dfec0371
Raoul v. R <vanruesc@users.noreply.github.com>
2021-02-18 23:47:16 +0100

    Delete SensitivitySettings

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -2,7 +2,6 @@ export { GeneralSettings } from "./GeneralSettings";
 export { KeyBindings } from "./KeyBindings";
 export { PointerSettings } from "./PointerSettings";
 export { RotationSettings } from "./RotationSettings";
-export { SensitivitySettings } from "./SensitivitySettings";
 export { TranslationSettings } from "./TranslationSettings";
 export { Settings } from "./Settings";
 export { ZoomSettings } from "./ZoomSettings";
