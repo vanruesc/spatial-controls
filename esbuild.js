f273f476d30f7851b3c6bb7516a4bb67aa5b15dd
Raoul v. R <vanruesc@users.noreply.github.com>
2021-02-21 14:48:52 +0100

    Reduce and simplify

--- esbuild.js
+++ esbuild.js
@@ -1,40 +1,40 @@
+import { createRequire } from "module";
 import esbuild from "esbuild";
-import path from "path";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-import { watch } from "chokidar";
-import { configs, srcDirs } from "./esbuild.config.js";
 
+const require = createRequire(import.meta.url);
+const pkg = require("./package");
+const date = (new Date()).toDateString();
+const production = (process.env.NODE_ENV === "production");
+const external = Object.keys(pkg.peerDependencies);
 const { argv } = yargs(hideBin(process.argv))
 	.option("watch", { alias: "w", type: "boolean" });
 
-async function build(f) {
+const banner = `/**
+ * ${pkg.name} v${pkg.version} build ${date}
+ * ${pkg.homepage}
+ * Copyright ${date.slice(-4)} ${pkg.author.name}
+ * @license ${pkg.license}
+ */`;
 
-	const t0 = Date.now();
+const configs = [{
+	entryPoints: ["demo/src/index.ts"],
+	outfile: "public/demo/index.js",
+	format: "iife",
+	bundle: true,
+	minify: production,
+	watch: argv.watch
+}, {
+	entryPoints: ["src/index.ts"],
+	outfile: `dist/${pkg.name}.js`,
+	format: "esm",
+	bundle: true,
+	external,
+	banner
+}];
 
-	await Promise.all(configs.map(c => {
-
-		return (path.normalize(c.outfile) === f) ? null : esbuild.build(c)
-			.then(r => console.log(`Built ${c.outfile} in ${Date.now() - t0}ms`))
-			.catch(() => process.exit(1));
-
-	}));
-
-}
-
-if(argv.watch) {
-
-	console.clear();
-	console.log("Watching %s for changes…\n",
-		srcDirs.join(", ").replace(/, ([^,]*)$/, " and $1"));
-
-	watch(srcDirs).on("change", f => {
-
-		console.clear();
-		void build(path.normalize(f))
-
-	});
-
-}
-
-void build();
+const t0 = Date.now();
+await Promise.all(configs.map(c => esbuild.build(c)
+	.then(() => console.log(`Built ${c.outfile} in ${Date.now() - t0}ms`))
+	.catch(() => process.exit(1))));
