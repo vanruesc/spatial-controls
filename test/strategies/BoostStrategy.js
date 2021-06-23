77ba9586ea4205a30610778dfce985b09f5d3318
Raoul v. R <vanruesc@users.noreply.github.com>
2021-06-23 16:19:15 +0200

    Create BoostStrategy.js

---
+++ test/strategies/BoostStrategy.js
@@ -0,0 +1,10 @@
+import test from "ava";
+import { BoostStrategy } from "../../dist/spatial-controls.js";
+
+test("can be instantiated", t => {
+
+	const object = new BoostStrategy();
+
+	t.pass();
+
+});
