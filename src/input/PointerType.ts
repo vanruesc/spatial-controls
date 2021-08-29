1510980be2df3ff0e43a13d09f01cefcb46c1434
Raoul v. R <vanruesc@users.noreply.github.com>
2021-08-29 16:32:50 +0200

    Create PointerType.ts

---
+++ src/input/PointerType.ts
@@ -0,0 +1,13 @@
+/**
+ * An enumeration of pointer types.
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/pointerType}
+ */
+
+export enum PointerType {
+
+	MOUSE = "mouse",
+	PEN = "pen",
+	TOUCH = "touch"
+
+}
