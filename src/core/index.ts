0524d1d2e947e221d5876b1e8d3d719c6c70acda
Raoul v. R <vanruesc@users.noreply.github.com>
2021-08-29 16:32:43 +0200

    Update index.ts

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,6 +2,5 @@ export { Action } from "./Action";
 export { ControlMode } from "./ControlMode";
 export { Direction } from "./Direction";
 export { Disposable } from "./Disposable";
-export { PointerBehaviour } from "./PointerBehaviour";
 export { SpatialControls } from "./SpatialControls";
 export { Updatable } from "./Updatable";
