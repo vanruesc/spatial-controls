9b2fd901de581c2220e71a583b65d4cf8171d802
Raoul v. R <vanruesc@users.noreply.github.com>
2021-09-30 22:17:25 +0200

    Update index.ts

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -1,4 +1,5 @@
 export { BoostStrategy } from "./BoostStrategy";
 export { MovementStrategy } from "./MovementStrategy";
+export { RotationStrategy } from "./RotationStrategy";
 export { Strategy } from "./Strategy";
 export { ZoomStrategy } from "./ZoomStrategy";
