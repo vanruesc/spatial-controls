6d63c0def2de6226d2e441c2adb10eca82cdd054
Raoul v. R <vanruesc@users.noreply.github.com>
2021-09-30 22:12:12 +0200

    Add optional event parameter

--- src/strategies/Strategy.ts
+++ src/strategies/Strategy.ts
@@ -8,8 +8,9 @@ export interface Strategy {
 	 * Executes this strategy.
 	 *
 	 * @param flag - A flag.
+	 * @param event - An event.
 	 */
 
-	execute(flag: boolean): void;
+	execute(flag: boolean, event?: Event): void;
 
 }
