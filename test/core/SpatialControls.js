2c20e41f42ae4de36a664ea6d6cffd1a7f8c9498
Raoul v. R <vanruesc@users.noreply.github.com>
2021-10-28 22:32:21 +0200

    Update tests

--- test/core/SpatialControls.js
+++ test/core/SpatialControls.js
@@ -4,18 +4,26 @@ import { SpatialControls } from "../../dist/spatial-controls.js";
 
 test("can be instantiated", t => {
 
-	const object = new SpatialControls(new Vector3(), new Quaternion(), null);
-
+	const object = new SpatialControls();
 	t.pass();
 
 });
 
 test("can copy another instance", t => {
 
-	const controls1 = new SpatialControls(new Vector3(), new Quaternion(), null);
-	const controls2 = new SpatialControls(new Vector3(), new Quaternion(), null);
+	const controls1 = new SpatialControls();
+	const controls2 = new SpatialControls();
 
 	controls1.copy(controls2);
+	t.pass();
+
+});
+
+test("can be cloned", t => {
+
+	const controls1 = new SpatialControls();
+	const controls2 = controls1.clone();
+
 	t.pass();
 
 });
