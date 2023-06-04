441066dd2ebfcb3797272b774b84e3215b24dccc
Raoul v. R <vanruesc@users.noreply.github.com>
2023-06-04 17:31:06 +0200

    Update esbuild.js

--- esbuild.js
+++ esbuild.js
@@ -1,12 +1,6 @@
-import { createRequire } from "module";
-import alias from "esbuild-plugin-alias";
+import pkg from "./package.json" assert { type: "json" };
 import esbuild from "esbuild";
-import path from "path";
-import url from "url";
 
-const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-const require = createRequire(import.meta.url);
-const pkg = require("./package");
 const date = new Date();
 const banner = `/**
  * ${pkg.name} v${pkg.version} build ${date.toDateString()}
@@ -15,28 +9,34 @@ const banner = `/**
  * @license ${pkg.license}
  */`;
 
-await esbuild.build({
-	entryPoints: ["src/index.ts"],
-	outfile: `dist/${pkg.name}.js`,
-	external: Object.keys(pkg.peerDependencies || {}),
+const lib = {
+	entryPoints: ["./src/index.ts"],
+	outfile: `./dist/${pkg.name}.js`,
 	banner: { js: banner },
+	external: ["three"],
 	logLevel: "info",
 	format: "esm",
 	bundle: true
-}).catch(() => process.exit(1));
+};
 
-await esbuild.build({
-	entryPoints: ["demo/src/index.ts"],
-	outdir: "public/demo",
+const demo = {
+	entryPoints: ["./demo/src/index.ts"],
+	outdir: "./public/demo",
 	minify: process.argv.includes("-m"),
-	watch: process.argv.includes("-w"),
 	logLevel: "info",
 	format: "iife",
 	target: "es6",
-	bundle: true,
-	plugins: [
-		alias({
-			[pkg.name]: path.resolve(__dirname, "./src/index.ts")
-		})
-	]
-}).catch(() => process.exit(1));
+	bundle: true
+};
+
+if(process.argv.includes("-w")) {
+
+	const ctxDemo = await esbuild.context(demo);
+	await ctxDemo.watch();
+
+} else {
+
+	await esbuild.build(lib);
+	await esbuild.build(demo);
+
+}
