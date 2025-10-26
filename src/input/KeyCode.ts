b4b2a65da4ec36ddbd0363e0107a82f1db3ab423
Raoul v. R. <vanruesc@outlook.de>
2025-10-26 15:22:39 +0100

    Update KeyCode.ts

--- src/input/KeyCode.ts
+++ src/input/KeyCode.ts
@@ -1,126 +1,126 @@
 /**
- * An enumeration of key codes.
+ * An enumeration of input keys.
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code
  * @group Input
  */
 
-export enum KeyCode {
+export type KeyCode = (
 
-	BACKSPACE = "Backspace",
-	TAB = "Tab",
-	ENTER = "Enter",
+	"Backspace" |
+	"Tab" |
+	"Enter" |
 
-	SHIFT_LEFT = "ShiftLeft",
-	SHIFT_RIGHT = "ShiftRight",
-	CTRL_LEFT = "ControlLeft",
-	CTRL_RIGHT = "ControlRight",
-	ALT_LEFT = "AltLeft",
-	ALT_RIGHT = "AltRight",
+	"ShiftLeft" |
+	"ShiftRight" |
+	"ControlLeft" |
+	"ControlRight" |
+	"AltLeft" |
+	"AltRight" |
 
-	PAUSE = "Pause",
-	CAPS_LOCK = "CapsLock",
-	ESCAPE = "Escape",
+	"Pause" |
+	"CapsLock" |
+	"Escape" |
 
-	SPACE = "Space",
-	PAGE_UP = "PageUp",
-	PAGE_DOWN = "PageDown",
-	END = "End",
-	HOME = "Home",
-	ARROW_LEFT = "ArrowLeft",
-	ARROW_UP = "ArrowUp",
-	ARROW_RIGHT = "ArrowRight",
-	ARROW_DOWN = "ArrowDown",
+	"Space" |
+	"PageUp" |
+	"PageDown" |
+	"End" |
+	"Home" |
+	"ArrowLeft" |
+	"ArrowUp" |
+	"ArrowRight" |
+	"ArrowDown" |
 
-	INSERT = "Insert",
-	DELETE = "Delete",
+	"Insert" |
+	"Delete" |
 
-	DIGIT_0 = "Digit0",
-	DIGIT_1 = "Digit1",
-	DIGIT_2 = "Digit2",
-	DIGIT_3 = "Digit3",
-	DIGIT_4 = "Digit4",
-	DIGIT_5 = "Digit5",
-	DIGIT_6 = "Digit6",
-	DIGIT_7 = "Digit7",
-	DIGIT_8 = "Digit8",
-	DIGIT_9 = "Digit9",
+	"Digit0" |
+	"Digit1" |
+	"Digit2" |
+	"Digit3" |
+	"Digit4" |
+	"Digit5" |
+	"Digit6" |
+	"Digit7" |
+	"Digit8" |
+	"Digit9" |
 
-	KEY_A = "KeyA",
-	KEY_B = "KeyB",
-	KEY_C = "KeyC",
-	KEY_D = "KeyD",
-	KEY_E = "KeyE",
-	KEY_F = "KeyF",
-	KEY_G = "KeyG",
-	KEY_H = "KeyH",
-	KEY_I = "KeyI",
-	KEY_J = "KeyJ",
-	KEY_K = "KeyK",
-	KEY_L = "KeyL",
-	KEY_M = "KeyM",
-	KEY_N = "KeyN",
-	KEY_O = "KeyO",
-	KEY_P = "KeyP",
-	KEY_Q = "KeyQ",
-	KEY_R = "KeyR",
-	KEY_S = "KeyS",
-	KEY_T = "KeyT",
-	KEY_U = "KeyU",
-	KEY_V = "KeyV",
-	KEY_W = "KeyW",
-	KEY_X = "KeyX",
-	KEY_Y = "KeyY",
-	KEY_Z = "KeyZ",
+	"KeyA" |
+	"KeyB" |
+	"KeyC" |
+	"KeyD" |
+	"KeyE" |
+	"KeyF" |
+	"KeyG" |
+	"KeyH" |
+	"KeyI" |
+	"KeyJ" |
+	"KeyK" |
+	"KeyL" |
+	"KeyM" |
+	"KeyN" |
+	"KeyO" |
+	"KeyP" |
+	"KeyQ" |
+	"KeyR" |
+	"KeyS" |
+	"KeyT" |
+	"KeyU" |
+	"KeyV" |
+	"KeyW" |
+	"KeyX" |
+	"KeyY" |
+	"KeyZ" |
 
-	OS_LEFT = "OSLeft",
-	OS_RIGHT = "OSRight",
-	META_LEFT = "MetaLeft",
-	META_RIGHT = "MetaRight",
-	MEDIA_SELECT = "MediaSelect",
+	"OSLeft" |
+	"OSRight" |
+	"MetaLeft" |
+	"MetaRight" |
+	"MediaSelect" |
 
-	NUMPAD_0 = "Numpad0",
-	NUMPAD_1 = "Numpad1",
-	NUMPAD_2 = "Numpad2",
-	NUMPAD_3 = "Numpad3",
-	NUMPAD_4 = "Numpad4",
-	NUMPAD_5 = "Numpad5",
-	NUMPAD_6 = "Numpad6",
-	NUMPAD_7 = "Numpad7",
-	NUMPAD_8 = "Numpad8",
-	NUMPAD_9 = "Numpad9",
-	NUMPAD_MULTIPLY = "NumpadMultiply",
-	NUMPAD_ADD = "NumpadAdd",
-	NUMPAD_SUBTRACT = "NumpadSubtract",
-	NUMPAD_DECIMAL = "NumpadDecimal",
-	NUMPAD_DIVIDE = "NumpadDivide",
+	"Numpad0" |
+	"Numpad1" |
+	"Numpad2" |
+	"Numpad3" |
+	"Numpad4" |
+	"Numpad5" |
+	"Numpad6" |
+	"Numpad7" |
+	"Numpad8" |
+	"Numpad9" |
+	"NumpadMultiply" |
+	"NumpadAdd" |
+	"NumpadSubtract" |
+	"NumpadDecimal" |
+	"NumpadDivide" |
 
-	F1 = "F1",
-	F2 = "F2",
-	F3 = "F3",
-	F4 = "F4",
-	F5 = "F5",
-	F6 = "F6",
-	F7 = "F7",
-	F8 = "F8",
-	F9 = "F9",
-	F10 = "F10",
-	F11 = "F11",
-	F12 = "F12",
+	"F1" |
+	"F2" |
+	"F3" |
+	"F4" |
+	"F5" |
+	"F6" |
+	"F7" |
+	"F8" |
+	"F9" |
+	"F10" |
+	"F11" |
+	"F12" |
 
-	NUM_LOCK = "NumLock",
-	SCROLL_LOCK = "ScrollLock",
+	"NumLock" |
+	"ScrollLock" |
 
-	SEMICOLON = "Semicolon",
-	EQUAL = "Equal",
-	COMMA = "Comma",
-	MINUS = "Minus",
-	PERIOD = "Period",
-	SLASH = "Slash",
-	BACKQUOTE = "Backquote",
+	"Semicolon" |
+	"Equal" |
+	"Comma" |
+	"Minus" |
+	"Period" |
+	"Slash" |
+	"Backquote" |
 
-	BRACKET_LEFT = "BracketLeft",
-	BRACKET_RIGHT = "BracketRight",
-	BACKSLASH = "Backslash"
+	"BracketLeft" |
+	"BracketRight" |
+	"Backslash"
 
-}
+);
