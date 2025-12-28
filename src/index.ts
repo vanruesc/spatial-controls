8b1158141f5d9c576df2942c727a1271f7fd8fc8
Raoul v. R. <vanruesc@outlook.de>
2025-12-28 16:20:02 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from "./core/index.js";
+export * from "./events/index.js";
 export * from "./input/index.js";
 export * from "./managers/index.js";
 export * from "./math/index.js";
 export * from "./settings/index.js";
-export * from "./strategies/index.js";
