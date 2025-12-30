98a0c383d8feec68ab4a476ad9995048ced77078
Raoul v. R. <vanruesc@outlook.de>
2025-12-30 22:55:52 +0100

    Convert to class and add modifierCount

--- src/core/BoundAction.ts
+++ src/core/BoundAction.ts
@@ -1,13 +1,29 @@
 import { Action } from "./Action.js";
 
 /**
+ * Counts the number of enabled flags in the given bitmask.
+ *
+ * @param bitmask - A bitmask.
+ * @return The number of enabled flags.
+ * @internal
+ */
+
+function getEnabledFlagCount(bitmask: number): number {
+
+	let n = bitmask - ((bitmask >>> 1) & 0x55555555);
+	n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+	return ((n + (n >>> 4) & 0xF0F0F0F) * 0x1010101) >>> 24;
+
+}
+
+/**
  * A bound action with optional modifiers.
  *
  * @group Core
  * @internal
  */
 
-export interface BoundAction {
+export class BoundAction {
 
 	/**
 	 * The action.
@@ -20,5 +36,26 @@ export interface BoundAction {
 	 */
 
 	readonly modifiers: number;
+
+	/**
+	 * The number of enabled modifier flags.
+	 */
+
+	readonly modifierCount: number;
+
+	/**
+	 * Constructs a new bound action.
+	 *
+	 * @param action - An action.
+	 * @param modifiers - Required modifiers stored as a bitmask.
+	 */
+
+	constructor(action: Action, modifiers: number) {
+
+		this.action = action;
+		this.modifiers = modifiers;
+		this.modifierCount = getEnabledFlagCount(modifiers);
+
+	}
 
 }
