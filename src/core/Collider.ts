1a0e2d4d5f7933a5d1c21a468b7b6ec2ebdd5d67
Raoul v. R. <vanruesc@outlook.de>
2026-01-03 16:42:17 +0100

    Update Collider

--- src/core/Collider.ts
+++ src/core/Collider.ts
@@ -3,11 +3,13 @@ import { Vector3 } from "three";
 /**
  * Performs a collision check with the given position and view direction.
  *
+ * @ignore
  * @experimental
  * @group Core
  * @param position - The position.
  * @param direction - The view direction.
+ * @param velocity - The velocity.
  * @return The adjusted position.
  */
 
-export type Collider = (position: Vector3, direction: Vector3) => Vector3;
+export type Collider = (position: Vector3, direction: Vector3, velocity: number) => Vector3;
